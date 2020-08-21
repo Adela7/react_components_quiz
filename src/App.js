4a30b19f626fab3a4d052f59d696bd406fd156a7
Adelaide Atakora <adelaide.atakora@gmail.com>
2020-08-21 15:47:30 +0000

    Class components

--- src/App.js
+++ src/App.js
@@ -1,47 +1,47 @@
-import React from "react";
-import "./App.css";
-import faker from "faker";
+import React from 'react';
+import './App.css';
+import faker from 'faker';
 
-import ApprovalCard from "./ApprovalCard";
-import CommentDetail from "./CommentDetail";
+import ApprovalCard from './ApprovalCard';
+import CommentDetail from './CommentDetail';
 
 function componentBuilder() {
   let data = [
     {
-      author: "Dennis Obel",
-      timeAgo: "Today at 4:45PM",
-      commentText: "Good job",
+      author: 'Dennis Obel',
+      timeAgo: 'Today at 4:45PM',
+      commentText: 'Good job',
       img: faker.image.avatar(),
     },
     {
-      author: "Paul Solomon",
-      timeAgo: "Today at 5:45PM",
-      commentText: "Nice work",
+      author: 'Paul Solomon',
+      timeAgo: 'Today at 5:45PM',
+      commentText: 'Nice work',
       img: faker.image.avatar(),
     },
     {
-      author: "Peter Nortey",
-      timeAgo: "Today at 6:45PM",
-      commentText: "Great stuff",
+      author: 'Peter Nortey',
+      timeAgo: 'Today at 6:45PM',
+      commentText: 'Great stuff',
       img: faker.image.avatar(),
     },
     {
-      author: "James Asafuah",
-      timeAgo: "Today at 7:45PM",
-      commentText: "Commendable",
+      author: 'James Asafuah',
+      timeAgo: 'Today at 7:45PM',
+      commentText: 'Commendable',
       img: faker.image.avatar(),
     },
     {
-      author: "Persius Osei-Tutu",
-      timeAgo: "Today at 7:45PM",
-      commentText: "Commendable",
+      author: 'Persius Osei-Tutu',
+      timeAgo: 'Today at 7:45PM',
+      commentText: 'Commendable',
       img: faker.image.avatar(),
     },
   ];
 
   return data
     .filter((element) => {
-      return element.author.includes("P");
+      return element.author.includes('P');
     })
     .map((element) => {
       return (
@@ -59,15 +59,17 @@ function componentBuilder() {
 
 // convert to class-based component
 
-function App() {
-  return (
-    <div className="ui container">
-      <ApprovalCard>
-        <p>Are you sure?</p>
-      </ApprovalCard>
-      {componentBuilder()}
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div className="ui container">
+        <ApprovalCard>
+          <p>Are you sure?</p>
+        </ApprovalCard>
+        {componentBuilder()}
+      </div>
+    );
+  }
 }
 
 export default App;
