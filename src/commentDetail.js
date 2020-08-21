025c02c57367f53d41dbef9cc7a4afbf45618c8b
Adelaide Atakora <adelaide.atakora@gmail.com>
2020-08-21 15:35:43 +0000

    Class components

---
+++ src/commentDetail.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+// convert to class-based component
+class ApprovalCard extends Component {
+  // const CommentDetail = (props) => {
+  render() {
+    return (
+      <div className="comment">
+        <a href="/" className="avatar">
+          <img alt="avatar" src={props.img} />
+        </a>
+        <div className="content">
+          <a href="/" className="author">
+            {props.author}
+          </a>
+          <div className="metadata">
+            <span className="date">{props.timeAgo}</span>
+          </div>
+          <div className="text">{props.commentText}</div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default CommentDetail;
